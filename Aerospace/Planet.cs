15950e4d701c52ddce8d0ba997a4e2e66c5d3f15
Andreas Schmitt <andreas@vacuumbreather.de>
2022-10-09 17:50:56 +0200

    Add planet data structure

---
+++ Aerospace/Planet.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Aerospace;
+
+internal struct Planet
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("positionIndex")]
+    public int Index { get; set; }
+
+    [JsonPropertyName("habitable")]
+    public bool IsHabitable { get; set; }
+
+    [JsonPropertyName("diameter")]
+    public double Diameter { get; set; }
+
+    [JsonPropertyName("averageTemperature")]
+    public double AverageTemperature { get; set; }
+
+    [JsonPropertyName("distanceFromEarth")]
+    public double DistanceFromEarth { get; set; }
+
+    [JsonPropertyName("isDwarf")]
+    public bool IsDwarf { get; set; }
+}
