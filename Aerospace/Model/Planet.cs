c6ee568c9102dc92d1585ccb9607fae8e7c1a1f8
Andreas Schmitt <andreas@vacuumbreather.de>
2022-10-11 21:24:07 +0200

    Add distance calculations

--- Aerospace/Model/Planet.cs
+++ Aerospace/Model/Planet.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System;
+using System.Text.Json.Serialization;
 
 namespace Aerospace.Model;
 
@@ -24,6 +25,20 @@ internal struct Planet
 
     [JsonPropertyName("isDwarf")]
     public bool IsDwarf { get; set; }
+
+    public static double CalculateDistanceBetween(Planet firstPlanet, Planet secondPlanet)
+    {
+        if ((firstPlanet.Index == 3) || (secondPlanet.Index == 3) ||
+            ((firstPlanet.Index > 3) && (secondPlanet.Index > 3)) ||
+            ((firstPlanet.Index < 3) && (secondPlanet.Index < 3)))
+        {
+            // Either include earth or on same side of earth in regards to the sun.
+            return Math.Abs(firstPlanet.DistanceFromEarth - secondPlanet.DistanceFromEarth);
+        }
+
+        // Either side of Earth in regards to the sun.
+        return firstPlanet.DistanceFromEarth + secondPlanet.DistanceFromEarth;
+    }
 
     public override string ToString() => Name;
 }
--- Aerospace/Model/SpacecraftJourney.cs
+++ Aerospace/Model/SpacecraftJourney.cs
@@ -9,6 +9,8 @@ internal class SpacecraftJourney : PropertyChangedBase
 {
     #region Constants and Fields
 
+    private readonly Model model;
+
     private string name;
     private int numPassengers;
     private Spacecraft spacecraft;
@@ -19,6 +21,7 @@ internal class SpacecraftJourney : PropertyChangedBase
 
     public SpacecraftJourney(Model model)
     {
+        this.model = model;
         name = "New Journey";
         spacecraft = model.Spacecrafts.First();
         Route.Add(model.Planets.First(planet => planet.Index == 3));
@@ -33,6 +36,18 @@ internal class SpacecraftJourney : PropertyChangedBase
 
     #region Public Properties
 
+    [JsonIgnore]
+    public double AdjustedMaxTravelDistance
+    {
+        get
+        {
+            double efficiency = (spacecraft.Capacity - NumPassengers) / (double) spacecraft.Capacity;
+            double distanceFactor = 0.7 + (0.3 * efficiency);
+
+            return spacecraft.MaxTravelDistance * distanceFactor;
+        }
+    }
+
     [JsonPropertyName("name")]
     public string Name
     {
@@ -44,10 +59,15 @@ internal class SpacecraftJourney : PropertyChangedBase
     public int NumPassengers
     {
         get => numPassengers;
-        set => Set(ref numPassengers, value);
+        set
+        {
+            Set(ref numPassengers, value);
+            NotifyOfPropertyChange(nameof(AdjustedMaxTravelDistance));
+        }
     }
 
-    public double RemainingTravelDistance => 0;
+    [JsonIgnore]
+    public double RemainingTravelDistance => AdjustedMaxTravelDistance - TotalTravelDistance;
 
     [JsonPropertyName("route")]
     public ObservableCollection<Planet> Route { get; } = new();
@@ -56,10 +76,33 @@ internal class SpacecraftJourney : PropertyChangedBase
     public Spacecraft Spacecraft
     {
         get => spacecraft;
-        set => Set(ref spacecraft, value);
+        set
+        {
+            Set(ref spacecraft, value);
+            NotifyOfPropertyChange(nameof(AdjustedMaxTravelDistance));
+        }
     }
 
-    public double TotalTravelDistance => 0;
+    [JsonIgnore]
+    public double TotalTravelDistance
+    {
+        get
+        {
+            var totalDistance = 0.0;
+            Planet earth = Route.First(planet => planet.Index == 3);
+            Planet previousPlanet = earth;
+
+            foreach (Planet planet in Route)
+            {
+                totalDistance += Planet.CalculateDistanceBetween(previousPlanet, planet);
+                previousPlanet = planet;
+            }
+
+            totalDistance += Planet.CalculateDistanceBetween(previousPlanet, earth);
+
+            return totalDistance;
+        }
+    }
 
     #endregion
 }
